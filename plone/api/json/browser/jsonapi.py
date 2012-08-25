cd556c7fd9a48133f9090def798594c0d58acaf8
Fabiano Weimar dos Santos <xiru@xiru.org>
2012-08-25 13:13:03 -0300

    Initial implementation for the view, that already works for simples method calls but still needs work for method that returns objects or receive complex data structures.

--- plone/api/json/browser/jsonapi.py
+++ plone/api/json/browser/jsonapi.py
@@ -1,18 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import json
+from zope.interface import implements
+from zope.publisher.interfaces import IPublishTraverse
+from zExceptions import NotFound
 from five import grok
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone import api
 
 class JSONAPI(grok.View):
     """A view that exposes plone.api using JSON.
     """
 
-    grok.name('jsonapi')
+    implements(IPublishTraverse)
+
     grok.context(INavigationRoot)
+    grok.name('jsonapi')
     grok.require('cmf.ManagePortal')
-    
-    def render(self):
-        """foobar
+
+    apimod = None
+    apimet = None
+
+    def publishTraverse(self, request, name):
+        if self.apimod is None and name in ('portal', 'content', 'user', 'group'):
+            self.apimod = name
+            return self
+        if self.apimod is not None and self.apimet is None:
+            module = getattr(api, self.apimod)
+            method = getattr(module, name, None)
+            if method is not None:
+                self.apimet = name
+                return self
+        raise NotFound()
+
+    def render(self, method):
         """
-        return json.dumps('foobar')
+        """
+        request = self.request
+        method = getattr(getattr(api, self.apimod), self.apimet)
+        return json.dumps(method(**request.form))
