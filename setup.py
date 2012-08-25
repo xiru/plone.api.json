0865ca78a54f3959cbd6989bf044e99a5242dd71
Fabiano Weimar dos Santos <xiru@xiru.org>
2012-08-25 09:53:51 -0300

    Initial version of plone.api.json, generated using Zopeskel.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plone.api.json',
+      version=version,
+      description="The JSON version of plone.api",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Fabiano Weimar dos Santos',
+      author_email='xiru@xiru.org',
+      url='https://github.com/xiru/plone.api.json',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.api'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.api',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
